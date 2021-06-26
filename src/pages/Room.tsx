211fe8b1083dae9173149f3392787a7b354a9db8
denysLerroan <lerroandeco@gmail.com>
2021-06-25 22:35:48 -0300

    5th day: isAnswered and isHighlighted function on questions

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -100,8 +100,11 @@ export function Room(){
                 key={question.id}
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighLighted={question.isHighLighted}
               >
-                <button
+                {!question.isAnswered && (
+                  <button
                   className={`like-button ${question.likeId ? 'liked' : ''}`}
                   type="button"
                   aria-label="Marcar como gostei"
@@ -113,6 +116,7 @@ export function Room(){
                   </svg>
 
                 </button>
+                )}
               </Question>
             )
           })}
